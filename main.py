0e2ef43c0e50b09afd682f4d62918c11de2e9d9a
garr80 <1137577185@qq.com>
2018-08-09 21:16:38 +0800

    addtest

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ motor3 = (29, 31)
 motor4 = (33, 35)
 motor = (motor1, motor2, motor3, motor4)
 direction = [0, 0, 0, 0]
-dc = [0, 0, 0, 0]
+dc = [50, 50, 50, 50]
 direction_old = [0, 0, 0, 0]
-dc_old= [0, 0, 0, 0]
+dc_old= [50, 50, 50, 50]
 
 
-p1 = GPIO.PWM(motor[1], 50)
-p2 = GPIO.PWM(motor[1], 50)
-p3 = GPIO.PWM(motor[1], 50)
-p4 = GPIO.PWM(motor[1], 50)
+p1 = GPIO.PWM(motor[0][0], 50)
+p2 = GPIO.PWM(motor[1][0], 50)
+p3 = GPIO.PWM(motor[2][0], 50)
+p4 = GPIO.PWM(motor[3][0], 50)
 p = [p1, p2, p3, p4]
 
 pwm.pwm_init()
