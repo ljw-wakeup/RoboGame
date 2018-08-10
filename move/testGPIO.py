d4956a71e81466bfc154c7bb5b8d2f415eecc00f
ljw-wakeup <liw0105@mail.ustc.edu.cn>
2018-08-10 17:13:39 +0800

    222

--- move/testGPIO.py
+++ move/testGPIO.py
@@ -1,5 +1,12 @@
 import RPi.GPIO as GPIO
 
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT)
-GPIO.output(11, GPIO.HIGH)
+GPIO.setup(12, GPIO.OUT)
+GPIO.output(12, GPIO.HIGH)
+try :
+	while true :
+		pass
+finally: 
+	GPIO.cleanup()
+
