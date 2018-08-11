dabd7bab12dcf8517bf9a6a50f7c30c8e70934ef
garr80 <1137577185@qq.com>
2018-08-11 16:16:54 +0800

    pwmtest

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 import pwm
-import RPi.GPIO as GPIO
+import RPI.GPIO as GPIO
 import pid
 
-motor1 = (7, 11)
-motor2 = (13, 15)
-motor3 = (29, 31)
-motor4 = (33, 35)
+GPIO.setwarnings(False)
+motor1 = (31, 32)
+motor2 = (33, 36)
+motor3 = (35, 38)
+motor4 = (37, 40)
 motor = (motor1, motor2, motor3, motor4)
 direction = [0, 0, 0, 0]
 dc = [50, 50, 50, 50]
 direction_old = [0, 0, 0, 0]
 dc_old= [50, 50, 50, 50]
 
-GPIO.cleanup()
-pwm.pwm_init(motor)
+
 p1 = GPIO.PWM(motor[0][0], 50)
 p2 = GPIO.PWM(motor[1][0], 50)
 p3 = GPIO.PWM(motor[2][0], 50)
 p4 = GPIO.PWM(motor[3][0], 50)
 p = [p1, p2, p3, p4]
 
+pwm.pwm_init()
 pwm.pwm_direction_control(p,direction,motor)
 try:
     while True:
--- pwm.py
+++ pwm.py
@@ -1,4 +1,4 @@
-import RPi.GPIO as GPIO
+import RPI.GPIO as GPIO
 import time
 import pid
 
